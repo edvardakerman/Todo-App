4286222027180ad2777ce953dcb25b03b20d2e76
edvardakerman <ed@sagovagen.se>
2021-10-05 16:53:39 +0200

    new filter function

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,35 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-const lists = [
+const Todolist = [
   {
-    'id': 1,
-    'task': 'Köp mjölk',
-    'content': 'Gå till ica och köp två liter mjölk'
+    id: 1,
+    task: 'Köp mjölk',
+    content: 'Gå till ica och köp två liter mjölk'
   },
   {
-    'id': 2,
-    'task': 'Köp kanelbullar',
-    'content': 'Gå till ica och köp kanelbullar'
+    id: 2,
+    task: 'Köp kanelbullar',
+    content: 'Gå till ica och köp kanelbullar'
   },
 ]
 
 // get all
 router.get('/api/todo', function (req, res, next) {
   console.log(req.query.query);
-  res.json(lists);
+  res.json(Todolist);
 });
 
 // post
 router.post('/api/todo', function (req, res, next) {
   lists.push(req.body);
-  res.json(lists);
+  res.json(Todolist);
 });
 
 // get one
 router.get('/api/todo/:id', function (req, res, next) {
-  var id = req.params.id - 1;
-  res.json(lists[id]);
+  const itemId = parseInt(req.params.id);
+  const item = Todolist.find((item) => {
+    return item.id === itemId
+  });
+  res.json(item);
 });
 
 module.exports = router;
