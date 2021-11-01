5abb381fb0c4d64da91d5ff0a46c8b4cb07d3847
edvardakerman <ed@sagovagen.se>
2021-11-01 15:43:51 +0100

    create todo component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import TodoList from "./components/TodoList";
+import CreateTodo from "./components/CreateTodo";
 
 const App = () => {
   return (
     <div>
       <Route exact path="/">
         <TodoList />
+      </Route>
+      <Route exact path="/Create">
+        <CreateTodo />
       </Route>
     </div>
   );
---
+++ client/src/components/CreateTodo.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const CreateTodo = () => {
+  const [formFields, setFormFields] = useState(null);
+  const history = useHistory();
+
+  const handleChange = (value, fieldId) => {
+    const payload = { ...formFields };
+    payload[fieldId] = value;
+    setFormFields(payload);
+  };
+
+  console.log(formFields);
+
+  const changeHandler = (event) =>
+    handleChange(event.target.value, event.target.id);
+
+  const handleCreateTodo = async (event) => {
+    event.preventDefault();
+    const url = "http://localhost:3000/api/todo";
+    const obj = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify(formFields),
+    };
+    const response = await fetch(url, obj);
+    if (!response.ok) {
+      throw new Error("Something went wrong!");
+    }
+    history.push(`/`);
+  };
+
+  const handleCancel = () => {
+    history.push(`/`);
+  };
+
+  return (
+    <section>
+      <form onSubmit={handleCreateTodo} onChange={changeHandler}>
+        <h1>New Todo</h1>
+        <input label="Title:" id="task_title" type="text" />
+        <input label="Description:" id="task_body" type="text" />
+        <input label="Id:" id="task_id" type="number" />
+        <div>
+          <button>Create</button>
+          <button onClick={handleCancel}>Cancel</button>
+        </div>
+      </form>
+    </section>
+  );
+};
+export default CreateTodo;
